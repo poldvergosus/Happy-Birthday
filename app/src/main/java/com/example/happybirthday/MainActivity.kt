789f29b10b6a951c20bf0a17960e67bcec237260
Poldvergosus <1polinapercel1@gmail.com>
2024-11-09 02:43:09 +0300

    Switch images

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.happybirthday
 
 import android.os.Bundle
+import android.widget.ImageSwitcher
 import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -37,14 +46,63 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-                    GreetingImage(stringResource(R.string.happy_day_text),
-                        stringResource(R.string.signature_text)
-                    )
+//                    GreetingImage(stringResource(R.string.happy_day_text),
+//                        stringResource(R.string.signature_text)
+//                    )
+                    ImageSwitcherScreen()
                 }
                 }
             }
         }
     }
+
+
+@Composable
+fun ImageSwitcherScreen(){
+    val images = listOf(R.drawable.androidparty, R.drawable._311459)
+    var currentImageIndex by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // для смены изображения
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth())
+        {
+            Button(onClick = {
+
+                    currentImageIndex = (currentImageIndex - 1 )
+                }
+            ) {
+                Text("Назад")
+            }
+
+            Button(
+                onClick = {
+
+                    currentImageIndex = (currentImageIndex + 1)
+                }
+            ) {
+                Text("Далее")
+            }
+        }
+        //для отображения текущего изображения
+        Image(
+            painter = painterResource(id = images[currentImageIndex]),
+            contentDescription = "Displayed Image",
+            modifier = Modifier
+                .border(2.dp, Color.Gray)
+                .background(Color.LightGray),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
 
 @Composable
 fun GreetingText(message: String = stringResource(R.string.happy_day_text) , from: String = stringResource(R.string.signature_text), modifier: Modifier = Modifier)
@@ -97,6 +155,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier =Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-    GreetingImage(stringResource(R.string.happy_day_text) , stringResource(R.string.signature_text))
+  //  GreetingImage(stringResource(R.string.happy_day_text) , stringResource(R.string.signature_text))
+        ImageSwitcherScreen()
     }
 }
