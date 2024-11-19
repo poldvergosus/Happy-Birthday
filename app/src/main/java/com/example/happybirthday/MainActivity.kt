93a64ef9704eacfdd00b56ed4911cff1eea4a2a3
Poldvergosus <1polinapercel1@gmail.com>
2024-11-19 19:18:21 +0300

    Switch activities

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.happybirthday
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageSwitcher
 import androidx.compose.foundation.Image
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,15 +23,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -46,116 +51,44 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-//                    GreetingImage(stringResource(R.string.happy_day_text),
-//                        stringResource(R.string.signature_text)
-//                    )
-                    ImageSwitcherScreen()
+
+                   Mainscreen()
                 }
                 }
             }
         }
     }
 
-
 @Composable
-fun ImageSwitcherScreen(){
-    val images = listOf(R.drawable.androidparty, R.drawable._311459)
-    var currentImageIndex by remember { mutableIntStateOf(0) }
+fun Mainscreen(){
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("")
+    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+    Column ( modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+        horizontalAlignment = Alignment.CenterHorizontally){
 
-        // для смены изображения
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth())
-        {
-            Button(onClick = {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Enter your text") },
+            modifier = Modifier.fillMaxWidth()
+        )
 
-                    currentImageIndex = (currentImageIndex - 1 )
-                }
-            ) {
-                Text("Назад")
+        Button(onClick = {
+            val intent = Intent(context, SecondActivity::class.java).apply {
+                putExtra("Text", text)
             }
-
-            Button(
-                onClick = {
-
-                    currentImageIndex = (currentImageIndex + 1)
-                }
-            ) {
-                Text("Далее")
-            }
+            context.startActivity(intent)
+        }) {
+            Text("Click")
         }
-        //для отображения текущего изображения
-        Image(
-            painter = painterResource(id = images[currentImageIndex]),
-            contentDescription = "Displayed Image",
-            modifier = Modifier
-                .border(2.dp, Color.Gray)
-                .background(Color.LightGray),
-            contentScale = ContentScale.Crop
-        )
-
     }
 }
 
-
+@Preview(showBackground = true, name = "amogus", showSystemUi = true)
 @Composable
-fun GreetingText(message: String = stringResource(R.string.happy_day_text) , from: String = stringResource(R.string.signature_text), modifier: Modifier = Modifier)
-{
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            color = Color.DarkGray,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier =Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    Box (modifier){
-        Image( // ПОРЯДОК ВАЖЕН!!!
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.6F
-        )
-        GreetingText(// Вызываем функцию с текстом
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
-
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-  //  GreetingImage(stringResource(R.string.happy_day_text) , stringResource(R.string.signature_text))
-        ImageSwitcherScreen()
-    }
+fun MainscreenPreview(){
+    Mainscreen()
 }
